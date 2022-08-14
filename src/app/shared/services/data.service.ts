9c1d5543151e23632595c825b5c413361fc3ccc1
yichu lau <yichu21@gmail.com>
2022-08-14 23:03:24 +0800

    code changes

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -8,41 +8,33 @@ export class DataService {
 
   constructor(private http:HttpClient) { }
 
+  // Get World All Data
   getSummaryData(){
     let url = 'https://disease.sh/v3/covid-19/all'
     return this.http.get(url);
   }
-
+   // Get World All Data
   getAllCountryData(){
     let url = 'https://disease.sh/v3/covid-19/countries'
     return this.http.get(url);
   }
 
+   // Get World All Historical Data fo last 15 days
   getHistorical(){
     let url = 'https://disease.sh/v3/covid-19/historical/all?lastdays=15';
     return this.http.get(url);
   }
   
+   // Get Malaysia from last 10 datas Data
   getMYCountryData(){
     let url = 'https://disease.sh/v3/covid-19/historical/my?lastdays=10'
     return this.http.get(url);
   }
-
+  // Get SouthEast Asia country Data
   getSouthEastAsiaData(){
     let url = 'https://disease.sh/v3/covid-19/countries/my%2Csg%2Cph%2Cidn%2Ctha%2Ckhm%2Cvnm'
     return this.http.get(url);
   }
 
-  getMYCountryDataToday(){
-    let url = 'https://disease.sh/v3/covid-19/countries/my?strict=true'
-    return this.http.get(url);
-  }
-  getMYCountryDataYesterday(){
-    let url = 'https://disease.sh/v3/covid-19/countries/my?yesterday=true&strict=true'
-    return this.http.get(url);
-  }
-  getMYCountryDataPrevious(){
-    let url = 'https://disease.sh/v3/covid-19/countries/my?twoDaysAgo=true&strict=true'
-    return this.http.get(url);
-  }
+
 }
